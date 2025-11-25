62e0b31291316a3600f2635e0e565de5c12be4b0
youssef_tag <yousseftag50@gmail.com>
2025-11-25 18:46:58 +0200

    added a private instance ot containa the current user and its role

--- src/models/CurrentUser.java
+++ src/models/CurrentUser.java
@@ -1,19 +1,24 @@
 package models;
 
 public class CurrentUser {
+    private static CurrentUser instance;
+
     private String username;
     private String role;
 
-    public CurrentUser(String username , String role){
+    private CurrentUser(String username, String role) {
         this.username = username;
         this.role = role;
     }
 
-    public String getUsername() {
-        return username;
+    public static void setInstance(String username, String role) {
+        instance = new CurrentUser(username, role);
     }
 
-    public String getRole() {
-        return role;
+    public static CurrentUser getInstance() {
+        return instance;
     }
+
+    public String getUsername() { return username; }
+    public String getRole() { return role; }
 }
