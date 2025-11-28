8e5fcb4824688f243e4aa7d013264972017458c9
youssef_tag <yousseftag50@gmail.com>
2025-11-28 18:02:44 +0200

    added indentation

--- src/services/AuthServices.java
+++ src/services/AuthServices.java
@@ -6,29 +6,29 @@ import java.io.IOException;
 
 public class AuthServices {
     public static String getUserRole(String username, String password) {
-    String filePath = "files/users.txt";
+        String filePath = "files/users.txt";
 
-    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
 
-        while ((line = br.readLine()) != null) {
-            if (line.trim().isEmpty()) continue;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().isEmpty()) continue;
 
-            String[] parts = line.split("\\|");
-            if (parts.length != 5) continue;
+                String[] parts = line.split("\\|");
+                if (parts.length != 5) continue;
 
-            String fileName = parts[1];
-            String filePassword = parts[3];
-            String role = parts[4];
+                String fileName = parts[1];
+                String filePassword = parts[3];
+                String role = parts[4];
 
-            if (fileName.equals(username) && filePassword.equals(password)) {
-                return role;
+                if (fileName.equals(username) && filePassword.equals(password)) {
+                    return role;
+                }
             }
-        }
 
-        } catch (IOException e) {
-            System.out.println(e);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            return null;
         }
-        return null;
-    }
 }
