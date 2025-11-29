783150dc1e972afb4afcfd3fbe0f53ad5b75545a
youssef_tag <yousseftag50@gmail.com>
2025-11-29 19:19:04 +0200

    switching scene function

--- src/views/login/Login.java
+++ src/views/login/Login.java
@@ -1,9 +1,16 @@
 package views.login;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
+
+import java.io.IOException;
+
 import controllers.LoginController;
 import view_utils.Alerts;
 
@@ -28,5 +35,14 @@ public class Login {
         } catch (SecurityException e) {
             Alerts.showAlert("the username or password may be wrong" , "login failed");
         }
+    }
+
+    @FXML
+    public void goToRegister() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/register/register.fxml"));
+        Stage stage = (Stage) usernameField.getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
