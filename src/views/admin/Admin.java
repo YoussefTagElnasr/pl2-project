45f7eb8021f9a16b79c58cf88cc973a5842e0e54
youssef_tag <yousseftag50@gmail.com>
2025-11-30 21:44:34 +0200

    adding the data from the text file to the table

--- src/views/admin/Admin.java
+++ src/views/admin/Admin.java
@@ -1,10 +1,44 @@
 package views.admin;
 import controllers.AdminController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.AdminRequest;
-
+import java.util.ArrayList;
 
 
 
 public class Admin {
-    
+    @FXML
+    private TableView<AdminRequest> requstsTable;
+    @FXML
+    private TableColumn<AdminRequest, String> customerColumn;
+    @FXML
+    private TableColumn<AdminRequest, String> emailColumn;
+    @FXML
+    private TableColumn<AdminRequest, String> detailColumn;
+    @FXML
+    private TableColumn<AdminRequest, String> statusColumn;
+    @FXML
+    private TableColumn<AdminRequest, Integer> priceColumn;
+    @FXML
+    private TableColumn<AdminRequest, String> readyDateColumn;
+
+    @FXML
+    public void initialize(){
+        customerColumn.setCellValueFactory(new PropertyValueFactory<>("customer"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        detailColumn.setCellValueFactory(new PropertyValueFactory<>("detail"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        readyDateColumn.setCellValueFactory(new PropertyValueFactory<>("readyDate"));
+
+        ArrayList<AdminRequest> requestsList = AdminController.getAdminRequests();
+        ObservableList<AdminRequest> observableRequests = FXCollections.observableArrayList(requestsList);
+
+        requstsTable.setItems(observableRequests);
+    }
 }
