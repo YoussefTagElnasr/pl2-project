1eb4111443b7d04658f84f66aea9c624151ae556
youssef_tag <yousseftag50@gmail.com>
2025-11-30 21:43:45 +0200

    function returns the role instead of nothing

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -3,12 +3,13 @@ import services.AuthServices;
 import models.CurrentUser;
 
 public class LoginController {
-    public static void handleLogin(String username ,String password) throws SecurityException {
+    public static String handleLogin(String username ,String password) throws SecurityException {
         String role = AuthServices.getUserRole(username, password);
         if (role != null) {
             CurrentUser.setInstance(username, role);
         } else {
             throw new SecurityException("role was null");
         }
+        return role;
     }
 }
