ecc7df5a01dc04f238b30ddce2e853ffd9177afe
youssef_tag <yousseftag50@gmail.com>
2025-12-03 17:27:06 +0200

    loading the users in the controller

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -2,11 +2,18 @@ package controllers;
 import java.util.ArrayList;
 import models.AdminRequest;
 import services.RequestServices;
+import services.UsersServices;
+import models.User;
 
 
 public class AdminController {
     public static ArrayList<AdminRequest> getAdminRequests(){
         ArrayList<AdminRequest> requests = RequestServices.getAllRequests();
         return requests;
+    }
+
+    public static ArrayList<User> getAllUsers(){
+        ArrayList<User> users = UsersServices.loadAllUsers();
+        return users;
     }
 }
