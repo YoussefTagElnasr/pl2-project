a64eab1243e2b3ecf889547c3a9f471b4f0eefeb
Alfie112 <youssefmahmed48@yahoo.com>
2025-12-04 15:50:46 +0200

    Update main.java
    - Removed unnecessary comments, updated the title to refer to the Login page

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            // Load FXML
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/login/login.fxml"));
-            // The controller is automatically picked from fx:controller in FXML
             Parent root = loader.load();
-
-            // Create scene
             Scene scene = new Scene(root);
             stage.setScene(scene);
-
-            // Stage settings
-            stage.setTitle("Project Manager - Messages");
+            stage.setTitle("Login");
             stage.setMaximized(true);
             stage.setResizable(true);
 
