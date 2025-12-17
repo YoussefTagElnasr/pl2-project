3c9fce03ccb55f255c992bf1ea7f1c0bb4909b3b
youssef_tag <yousseftag50@gmail.com>
2025-12-17 23:56:40 +0200

    calling requests from the controller

--- src/controllers/ServiceProviderController.java
+++ src/controllers/ServiceProviderController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import models.Request;
+
+import java.util.List;
+import services.RequestServices;
+
 public class ServiceProviderController {
-    
+    public static List<Request> loadSpRequests(){
+        List<Request> requests = RequestServices.getRequests("sent");
+        return requests;
+    }
 }
